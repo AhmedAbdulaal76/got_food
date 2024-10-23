689c17c72fad76c5d1f0cb876f28869146273e3a
Ahmed Abdulaal <ahmedabdulaal746@gmail.com>
2024-10-23 11:04:15 +0300

    fix double nav + added home header

--- lib/common/style/themes/themeText.dart
+++ lib/common/style/themes/themeText.dart
@@ -4,23 +4,45 @@ class ThemeTextStyles {
   static const titleLarge = TextStyle(
     fontWeight: FontWeight.bold,
     fontSize: 16,
+    color: Colors.black,
   );
   static const titleMedium = TextStyle(
     fontWeight: FontWeight.bold,
     fontSize: 14,
+    color: Colors.black,
   );
   static const titleSmall = TextStyle(
     fontWeight: FontWeight.bold,
     fontSize: 12,
+    color: Colors.black,
   );
   static const bodyLarge = TextStyle(
-    fontSize: 16,
+    fontSize: 20,
+    color: Colors.black,
   );
   static const bodyMedium = TextStyle(
-    fontSize: 14,
+    fontSize: 16,
+    color: Colors.black,
   );
   static const bodySmall = TextStyle(
-    fontSize: 12,
+    fontSize: 14,
+    color: Colors.black,
+  );
+
+  static const headLine1 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+  static const headLine2 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+  static const headLine3 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
   );
 
   static TextTheme get textTheme {
@@ -31,6 +53,9 @@ class ThemeTextStyles {
       bodyLarge: bodyLarge,
       bodyMedium: bodyMedium,
       bodySmall: bodySmall,
+      headlineLarge: headLine1,
+      headlineMedium: headLine2,
+      headlineSmall: headLine3,
     );
   }
 }
--- lib/features/home/home-view/widgets/homePage.dart
+++ lib/features/home/home-view/widgets/homePage.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:got_food/common/widgets/layout/customScaffold.dart';
+import 'package:got_food/features/home/home-view/widgets/homeHeader.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
+    return const CustomScaffold(
       title: "Home Page",
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Text('Go to Second Page'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsetsDirectional.all(16),
+          child: Column(
+            children: [
+              HomeHeader(),
+            ],
+          ),
         ),
       ),
     );
