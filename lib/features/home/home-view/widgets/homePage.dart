007b5542521125bb911247fdc4ffcc90d3bbf9ac
Ahmed Abdulaal <ahmedabdulaal746@gmail.com>
2024-10-27 12:08:40 +0300

    categories setup

--- lib/features/home/home-view/widgets/homePage.dart
+++ lib/features/home/home-view/widgets/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:got_food/common/style/themes/themeColors.dart';
 import 'package:got_food/common/widgets/layout/customScaffold.dart';
 import 'package:got_food/features/home/home-view/widgets/homeHeader.dart';
 import 'package:got_food/features/home/home-view/widgets/recipe-card/recipesCard.dart';
@@ -8,15 +9,52 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScaffold(
+    return CustomScaffold(
       title: "Home Page",
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsetsDirectional.all(16),
+          padding: const EdgeInsetsDirectional.all(16),
           child: Column(
             children: [
-              HomeHeader(),
-              RecipesCard(),
+              const HomeHeader(),
+              const RecipesCard(),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Categories',
+                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                          fontWeight: FontWeight.normal,
+                        ),
+                  ),
+                  Text('See All',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium
+                          ?.copyWith(color: ThemeColors.secondaryTextColor))
+                ],
+              ),
+              // show categories
+              const SizedBox(height: 16),
+              SizedBox(
+                height: 50,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsetsDirectional.only(end: 8),
+                      height: 20,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: ThemeColors.primaryColorLight,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
