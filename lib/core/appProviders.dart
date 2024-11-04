bdaf4e33807951d2a7a5dadcfb2fabd071bec40a
Ahmed Abdulaal <ahmedabdulaal746@gmail.com>
2024-11-04 23:53:55 +0300

    fixed imports

--- lib/core/appProviders.dart
+++ lib/core/appProviders.dart
@@ -1,9 +1,10 @@
-import 'package:got_food/common/providers/CategoryProvider.dart';
 import 'package:got_food/common/providers/bottomNavigationProvider.dart';
 import 'package:got_food/core/locator.dart';
 import 'package:got_food/features/home/home-view/homeViewModel.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+
+import '../common/providers/CategoryViewModel.dart';
 
 List<SingleChildWidget> get appProviders => [
       ChangeNotifierProvider(create: (_) => BottomNavigationProvider()),
--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -1,8 +1,8 @@
 import 'package:get_it/get_it.dart';
-import 'package:got_food/common/providers/CategoryProvider.dart';
 import 'package:got_food/common/services/category_service.dart';
 import 'package:got_food/features/home/home_service.dart';
 
+import '../common/providers/CategoryViewModel.dart';
 import '../features/home/home-view/homeViewModel.dart';
 
 final GetIt locator = GetIt.instance;
