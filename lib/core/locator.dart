50d158b099df5609374cfcca36efbe06857779cc
Ahmed Abdulaal <ahmedabdulaal746@gmail.com>
2024-11-05 19:42:45 +0300

    finished auth service

--- lib/core/locator.dart
+++ lib/core/locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:got_food/common/services/category_service.dart';
+import 'package:got_food/features/auth/auth_service.dart';
 import 'package:got_food/features/home/home_service.dart';
 
 import '../common/providers/CategoryViewModel.dart';
@@ -13,4 +14,5 @@ void setupLocator() {
   locator.registerLazySingleton(() => CategoryService());
   locator.registerLazySingleton(
       () => CategoryViewModel(locator<CategoryService>()));
+  locator.registerLazySingleton(() => AuthService());
 }
