5a86fc77709fa0b3d0a5b9816edcf73bdf3f8a87
TanKhalid <143603131+TanKhalid@users.noreply.github.com>
2024-11-29 15:57:01 +0300

    Profile page view done

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,6 +4,7 @@ import 'package:got_food/features/home/categories-view/widgets/categoriesPage.da
 import 'package:got_food/features/home/categorys-recipes-view/widgets/categoryDetailsPage.dart';
 import 'package:got_food/features/recipes/recipe-details-view/widgets/recipeDetailsPage.dart';
 
+import 'features/account/settingsPage.dart';
 import 'features/auth/login-view/widgets/loginPage.dart';
 import 'features/auth/register-view/widgets/registerPage.dart';
 import 'features/home/home-view/widgets/homePage.dart';
@@ -15,6 +16,7 @@ Map<String, Widget Function(BuildContext)> get routes => {
       '/login': (context) => const LoginPage(),
       '/signup': (context) => SignUpPage(),
       '/home/categories/details': (context) => const CategoryDetailsPage(),
-      '/profile': (context) => const ProfilePage(),
+      '/profile': (context) =>  ProfilePage(),
       '/recipes/recipe-details': (context) => const RecipeDetailsPage(),
+      '/profile/settings': (context) =>  SettingsPage()
     };
