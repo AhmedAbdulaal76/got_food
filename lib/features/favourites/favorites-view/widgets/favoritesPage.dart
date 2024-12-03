657d4239731641057c6b1e358c39638bbaa6feb9
Ahmed Abdulaal <ahmedabdulaal746@gmail.com>
2024-12-03 23:19:14 +0300

    enhanced UI

--- lib/features/favourites/favorites-view/widgets/favoritesPage.dart
+++ lib/features/favourites/favorites-view/widgets/favoritesPage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:got_food/common/widgets/layout/customScaffold.dart';
-import 'package:got_food/common/widgets/recipe-card/recipeImage.dart';
+import 'package:got_food/common/widgets/recipe-card/recipesCard.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../common/models/recipe.dart';
@@ -19,50 +19,58 @@ class _FavoritesPageState extends State<FavoritesPage> {
   @override
   void initState() {
     // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  void didChangeDependencies() {
-    // TODO: implement didChangeDependencies
-    final viewModel = Provider.of<FavoritesViewModel>(context);
+    final viewModel = Provider.of<FavoritesViewModel>(context, listen: false);
     if (supabase.auth.currentUser != null) {
       viewModel.fetchFavorites(supabase.auth.currentUser!.id);
       recipes = viewModel.fetchedRecipes;
     }
-    super.didChangeDependencies();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // if (supabase.auth.currentUser != null && ) {
+    final viewModel = Provider.of<FavoritesViewModel>(context);
+    // if (supabase.auth.currentUser != null) {
     //   viewModel.fetchFavorites(supabase.auth.currentUser!.id);
     //   recipes = viewModel.fetchedRecipes;
     // }
 
-    Widget content = Column(
-      children: [
-        (recipes.isEmpty)
-            ? const Column()
-            : Expanded(
-                child: GridView.builder(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 8,
-                    mainAxisSpacing: 8,
-                  ),
-                  itemCount: recipes.length,
-                  itemBuilder: (context, index) {
-                    return RecipeImage(
-                      recipe: recipes[index],
-                      setFullView: false,
-                    );
-                  },
-                ),
-              ),
-      ],
+    Widget content = Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          viewModel.isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : (recipes.isEmpty)
+                  ? const Center(child: Text('No favorite recipes yet'))
+                  : Expanded(
+                      child: GridView.builder(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 12),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 8,
+                          mainAxisSpacing: 8,
+                          childAspectRatio: 0.7,
+                        ),
+                        itemCount: recipes.length,
+                        itemBuilder: (context, index) {
+                          return RecipesCard(
+                            recipe: recipes[index],
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 10),
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 12, horizontal: 4),
+                          );
+                        },
+                      ),
+                    ),
+        ],
+      ),
     );
 
     return CustomScaffold(title: 'Favorite Recipes', body: content);
