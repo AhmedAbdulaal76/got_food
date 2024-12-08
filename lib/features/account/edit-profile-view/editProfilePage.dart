b6acddf989d2cabf3f4f3fb0b39b05e7b8d11046
Ahmed Abdulaal <ahmedabdulaal746@gmail.com>
2024-12-08 11:29:19 +0300

    fix updating img and name

--- lib/features/account/edit-profile-view/editProfilePage.dart
+++ lib/features/account/edit-profile-view/editProfilePage.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:got_food/common/widgets/layout/customScaffold.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -15,7 +16,6 @@ class EditProfilePage extends StatefulWidget {
 }
 
 class _EditProfilePageState extends State<EditProfilePage> {
-
   final TextEditingController _nameController = TextEditingController();
   File? _selectedImage;
   bool _isLoading = false;
@@ -50,13 +50,13 @@ class _EditProfilePageState extends State<EditProfilePage> {
           .from('profiles')
           .select()
           .eq('user_uuid', user.id)
-          .single();  // Get a single record
+          .single(); // Get a single record
 
       // Populate the name controller with the user's name
       _nameController.text = response['name'] ?? '';
       // Optionally, populate other fields like imageUrl if available
-     //_selectedImage = response['imageurl'] ?? '';
-        } catch (e) {
+      //_selectedImage = response['imageurl'] ?? '';
+    } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error fetching user details: $e')),
       );
@@ -84,7 +84,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
 
         // Reset inputs and navigate back
         _nameController.clear();
-        Navigator.pop(context, 'updated');
+        // Navigator.pop(context, 'updated');
       } catch (e) {
         print('Error updating profile: $e');
         ScaffoldMessenger.of(context).showSnackBar(
@@ -98,7 +98,8 @@ class _EditProfilePageState extends State<EditProfilePage> {
         final user = Supabase.instance.client.auth.currentUser;
         DateTime now = DateTime.now();
         // Upload the image to Supabase storage
-        final filePath = 'profile-images-${user?.id}_$now.jpg'; // Unique file path
+        final filePath =
+            'profile-images-${user?.id}_$now.jpg'; // Unique file path
         await Supabase.instance.client.storage
             .from('profiles') // Assuming 'profiles' is the storage bucket
             .upload(filePath, _selectedImage!);
@@ -121,10 +122,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
         );
       }
     }
-
   }
-
-
 
   @override
   void initState() {
@@ -139,70 +137,82 @@ class _EditProfilePageState extends State<EditProfilePage> {
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
           : Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: ListView(
-            children: [
-              const SizedBox(height: 20),
-              GestureDetector(
-                onTap: _pickImage,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ListView(
                   children: [
-                    _selectedImage != null
-                        ? CircleAvatar(
-                      radius: 90,
-                      backgroundImage: FileImage(_selectedImage!),
-                    )
-                        : CircleAvatar(
-                      radius: 90,
-                      backgroundColor: Theme.of(context)
-                          .colorScheme
-                          .primary
-                          .withOpacity(0.5),
-                      child: const Icon(Icons.account_circle, size: 50),
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: _pickImage,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          _selectedImage != null
+                              ? CircleAvatar(
+                                  radius: 90,
+                                  backgroundImage: FileImage(_selectedImage!),
+                                )
+                              : CircleAvatar(
+                                  radius: 90,
+                                  backgroundColor: Theme.of(context)
+                                      .colorScheme
+                                      .primary
+                                      .withOpacity(0.5),
+                                  child: const Icon(Icons.account_circle,
+                                      size: 50),
+                                ),
+                          const SizedBox(
+                              height:
+                                  8), // Space between the avatar and the text
+                          const Text(
+                            'Tap to Edit',
+                          ),
+                        ],
+                      ),
                     ),
-                    const SizedBox(height: 8), // Space between the avatar and the text
-                    const Text('Tap to Edit',),
+                    const SizedBox(height: 40),
+                    TextField(
+                      controller: _nameController,
+                      decoration:
+                          const InputDecoration(labelText: 'Updated Name'),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () async {
+                        try {
+                          // Call the profile update function
+                          await _updateProfile();
+                          final userViewModel = Provider.of<UserViewModel>(
+                              context,
+                              listen: false);
+                          await userViewModel.fetchUserDetails();
+
+                          // Clear the inputs
+                          // _nameController.clear();
+
+                          // show a snackbar
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Profile updated successfully')),
+                          );
+                          // Navigate back to the profile page
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, '/profile', (route) => false);
+                        } catch (e) {
+                          print("Error updating profile: $e");
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                                content: Text("Failed to update profile: $e")),
+                          );
+                        }
+                      },
+                      child: const Text('Update Profile'),
+                    ),
                   ],
                 ),
               ),
-              const SizedBox(height: 40),
-              TextField(
-                controller: _nameController,
-                decoration:
-                const InputDecoration(labelText: 'Updated Name'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  try {
-                    // Call the profile update function
-                    await _updateProfile();
-                    final userViewModel = Provider.of<UserViewModel>(context, listen: false);
-                    userViewModel.fetchUserDetails();
-
-                    // Clear the inputs
-                    _nameController.clear();
-
-                    // Navigate back to the profile page
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, '/profile', (route) => false);
-                  } catch (e) {
-                    print("Error updating profile: $e");
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                          content: Text("Failed to update profile: $e")),
-                    );
-                  }
-                },
-                child: const Text('Update Profile'),
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
       showBottomNav: true,
       showBackButton: true,
     );
